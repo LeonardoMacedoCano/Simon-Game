965dee344b9c516c89c0e16e2262b2faae92b6dc
LCano <leonardomacedocano@gmail.com>
2020-08-10 23:16:24 -0300

    add event click btnPower

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+  var
+    btnPower = document.getElementById('btnPower'),
+    btnStart = document.getElementById('btnStart'),
+    btnStrict = document.getElementById('btnStrict');
 
+  btnPower.addEventListener('click', function(){
+    if (this.checked){
+      document.getElementById('count').value = "000";
+      document.getElementById('count').style.opacity = "1";
+    }else{
+      document.getElementById('count').value = "888";
+      document.getElementById('count').style.opacity = "0.6";
+    }
+  })
 })
