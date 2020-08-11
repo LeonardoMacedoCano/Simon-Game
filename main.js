a20cc6b205b8adcdfec22813f65fa6918f8377d1
LCano <leonardomacedocano@gmail.com>
2020-08-11 00:00:35 -0300

    add on function to displayLevel

--- main.js
+++ main.js
@@ -12,14 +12,19 @@ document.addEventListener('DOMContentLoaded', function(){
     simonGame = new simon(),
     btnPower = document.getElementById('btnPower'),
     btnStart = document.getElementById('btnStart'),
-    btnStrict = document.getElementById('btnStrict');
+    btnStrict = document.getElementById('btnStrict'),
+    inputCount = document.getElementById('count'),
+    displayLevel ={
+      on: function(){
+        var numLevel = ("00" + simonGame.level).slice(-3);
+        inputCount.value = numLevel;
+        inputCount.style.opacity = "1";
+      }
+    };
 
   btnPower.addEventListener('click', function(){
     if (this.checked){
-      document.getElementById('count').value = "000";
-      document.getElementById('count').style.opacity = "1";
-
-      simonGame.init();
+      displayLevel.on();
     }else{
       document.getElementById('count').value = "888";
       document.getElementById('count').style.opacity = "0.6";
